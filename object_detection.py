20e69bb72b5a3d138cd72b78cd32bc722ef4da51
klein <m15072762954@163.com>
2019-03-11 17:26:44 +0800

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -1,12 +1,23 @@
-from darkflow.net.build import TFNet 
+# from darkflow.net.build import TFNet 
 import cv2 
+import sys, os
+import time
+#print(os.getcwd())
+# the path below should be change according to the path of darknet
+sys.path.append(os.path.join('/home/spl/chuyutensor/darknet/','python/'))
 
+import darknet as dn
+import pdb
 
 class tfnet():
     options = {"pbLoad": "built_graph/yolo-2c.pb", "metaLoad": "built_graph/yolo-2c.meta", "threshold": 0.1}
 
     def __init__(self, options = options):
-        self.tfnet = TFNet(options)
+        start = time.time()
+        self.net = dn.load_net(b"cfg/yolov3.cfg", b"yolov3.weights", 0)
+        self.meta = dn.load_meta(b"cfg/coco.data")
+        print(time.time() - start)
+        # self.tfnet = TFNet(options)
         self.results = None
         
     """process result, return two bboxes, one is persons, the other is bicycles"""
@@ -24,5 +35,6 @@ class tfnet():
 
     def object_detection(self, original_img):
         original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
-        self.results = self.tfnet.return_predict(original_img)
+        self.rseults = dn.detect(net, meta, b"data/bedroom.jpg")
+        # self.results = self.tfnet.return_predict(original_img)
         return self.result_process()
